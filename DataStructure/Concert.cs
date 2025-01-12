dc4262882054c5a6276e084708c042d0d3795b7d
FelixSFD <felixsfd@sobotics.org>
2025-01-12 18:23:41 +0100

    initial deployment

---
+++ DataStructure/Concert.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json.Serialization;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace LPCalendar.DataStructure;
+
+/// <summary>
+/// Represents a concert of Linkin Park
+/// </summary>
+[DynamoDBTable("Concerts")]
+public class Concert
+{
+    /// <summary>
+    /// UUID of the concert
+    /// </summary>
+    [DynamoDBHashKey]
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    /// <summary>
+    /// Time when the concert starts according to Ticketmaster.
+    /// </summary>
+    private DateTimeOffset? _postedStartTime;
+
+    /// <summary>
+    /// Time when the concert starts according to Ticketmaster. As string for DynamoDB. Use <see cref="PostedStartTimeValue"/> to get the date-object.
+    /// </summary>
+    [DynamoDBProperty]
+    [JsonPropertyName("postedStartTime")]
+    public string? PostedStartTime
+    {
+        get => _postedStartTime.ToString(); // Serialize to ISO 8601 string
+        set => _postedStartTime = string.IsNullOrEmpty(value) ? null : DateTimeOffset.Parse(value);
+    }
+
+    /// <summary>
+    /// Time when the concert starts according to Ticketmaster.
+    /// </summary>
+    [DynamoDBIgnore]
+    [JsonIgnore]
+    public DateTimeOffset? PostedStartTimeValue
+    {
+        get => _postedStartTime;
+        set => _postedStartTime = value;
+    }
+    
+    /// <summary>
+    /// Country of the venue
+    /// </summary>
+    [DynamoDBProperty]
+    [JsonPropertyName("country")]
+    public string Country { get; set; }
+    
+    /// <summary>
+    /// State where the venue is located in
+    /// </summary>
+    [DynamoDBProperty]
+    [JsonPropertyName("state")]
+    public string? State { get; set; }
+    
+    /// <summary>
+    /// City of the venue
+    /// </summary>
+    [DynamoDBProperty]
+    [JsonPropertyName("city")]
+    public string City { get; set; }
+    
+    /// <summary>
+    /// Name of the venue
+    /// </summary>
+    [DynamoDBProperty]
+    [JsonPropertyName("venue")]
+    public string? Venue { get; set; }
+}
---
+++ Lambda.AddConcert/src/Lambda.AddConcert/Function.cs
@@ -0,0 +1,67 @@
+using System.Net;
+using System.Runtime.Serialization;
+using System.Text.Json;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using LPCalendar.DataStructure;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace Lambda.AddConcert;
+
+public class Function
+{
+    private readonly IAmazonDynamoDB _dynamoDbClient = new AmazonDynamoDBClient();
+    private readonly IDynamoDBContext _dynamoDbContext;
+
+    public Function()
+    {
+        _dynamoDbContext = new DynamoDBContext(_dynamoDbClient);
+    }
+
+
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        if (request.Body == null)
+        {
+            return new APIGatewayProxyResponse()
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest,
+                Body = "Body not found"
+            };
+        }
+        
+        // Parse JSON
+        context.Logger.LogInformation("Start parsing JSON...");
+        var concert = MakeConcertFromJsonBody(request.Body);
+        context.Logger.LogInformation("Start writing to DB...");
+        await SaveConcert(concert);
+        context.Logger.LogInformation("Concert written to DB");
+        
+        var response = new APIGatewayProxyResponse()
+        {
+            StatusCode = 200,
+            Body = $"Successfully created concert in '{concert.City}' at '{concert.PostedStartTime.ToString()}'"
+        };
+
+        return response;
+    }
+
+
+    private Concert MakeConcertFromJsonBody(string json)
+    {
+        string guid = Guid.NewGuid().ToString();
+        Concert concert = JsonSerializer.Deserialize<Concert>(json) ?? throw new InvalidDataContractException("JSON could not be parsed to Concert!");
+        concert.Id = guid;
+        return concert;
+    }
+
+
+    private async Task SaveConcert(Concert concert)
+    {
+        await _dynamoDbContext.SaveAsync(concert);
+    }
+}
