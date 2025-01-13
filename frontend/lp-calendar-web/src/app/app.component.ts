b7a8ae99d718316d91282246ae93ebd82f4d87b3
FelixSFD <felixsfd@sobotics.org>
2025-01-13 12:04:32 +0100

    added angular frontend

---
+++ frontend/lp-calendar-web/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NgbModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'lp-calendar-web';
+}
