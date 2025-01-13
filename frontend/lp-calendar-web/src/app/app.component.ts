f767999242d528c3428c3438ba369c787ccecfa7
FelixSFD <felixsfd@sobotics.org>
2025-01-13 14:09:43 +0100

    added auth to angular app

--- frontend/lp-calendar-web/src/app/app.component.ts
+++ frontend/lp-calendar-web/src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {OidcSecurityService} from 'angular-auth-oidc-client';
+import {AsyncPipe, JsonPipe, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgbModule],
+  imports: [RouterOutlet, NgbModule, NgIf, AsyncPipe, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'lp-calendar-web';
+
+  private readonly oidcSecurityService = inject(OidcSecurityService);
+
+  configuration$ = this.oidcSecurityService.getConfiguration();
+
+  userData$ = this.oidcSecurityService.userData$;
+
+  isAuthenticated$ = false;
+
+  ngOnInit(): void {
+    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated }) => {
+      console.log('Authenticated:', isAuthenticated);
+    });
+  }
+
+  login(): void {
+    this.oidcSecurityService.authorize();
+  }
 }
---
+++ frontend/lp-calendar-web/src/app/auth/auth.config.ts
@@ -0,0 +1,15 @@
+import {LogLevel, PassedInitialConfig} from 'angular-auth-oidc-client';
+import {provideHttpClient} from '@angular/common/http';
+
+export const authConfig: PassedInitialConfig = {
+  config: {
+    authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_KPfToYS9T', // Replace with your Cognito domain
+    redirectUrl: 'http://localhost:4200/',
+    clientId: '1epkncdmjoklpcoa77pl17jatj',
+    scope: 'email openid profile', // Scopes allowed in the App Client
+    responseType: 'code', // Authorization Code Flow
+    silentRenew: true, // Enable silent token renewal
+    useRefreshToken: true, // Use refresh tokens to maintain the session
+    logLevel: LogLevel.Debug // Enable detailed logs for debugging
+  }
+}
