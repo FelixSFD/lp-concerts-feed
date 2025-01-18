9e9ffae4f2649514ad21bec4aadb9385568d7a5c
FelixSFD <felixsfd@sobotics.org>
2025-01-18 16:20:37 +0100

    fix config for Prod

--- frontend/lp-calendar-web/src/environments/environment.ts
+++ frontend/lp-calendar-web/src/environments/environment.ts
@@ -1,8 +1,8 @@
 
 export const environment = {
   apiCachedBaseUrl: "https://d17825cp6vwwpn.cloudfront.net",
-  apiNoCacheBaseUrl: "https://bfcczkmup5.execute-api.eu-central-1.amazonaws.com/Prod",
-  cognitoBaseUrl: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_KPfToYS9T",
+  apiNoCacheBaseUrl: "https://bfcczkmup5.execute-api.eu-central-1.amazonaws.com",
+  cognitoBaseUrl: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_Gb3bN77GE",
   cognitoClientId: "2q3mbq28urk0nduaecfnqf8c74",
   cognitoRedirectUrl: "https://lpcalendar.felixsfd.de/"
 };
