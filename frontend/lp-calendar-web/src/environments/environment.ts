dacf33c366a31e3b5a1804710bd7de00c564d494
FelixSFD <felixsfd@sobotics.org>
2025-01-19 14:52:46 +0100

    update prod API URL

--- frontend/lp-calendar-web/src/environments/environment.ts
+++ frontend/lp-calendar-web/src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
-  apiCachedBaseUrl: "https://d17825cp6vwwpn.cloudfront.net",
-  apiNoCacheBaseUrl: "https://bfcczkmup5.execute-api.eu-central-1.amazonaws.com",
+  apiCachedBaseUrl: "https://api.lpcalendar.felixsfd.de",
+  apiNoCacheBaseUrl: "https://api.lpcalendar.felixsfd.de",
   cognitoBaseUrl: "https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_Gb3bN77GE",
   cognitoClientId: "2q3mbq28urk0nduaecfnqf8c74",
   cognitoRedirectUrl: "https://lpcalendar.felixsfd.de/"
