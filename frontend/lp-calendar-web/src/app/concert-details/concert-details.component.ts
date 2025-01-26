d83c98c0ccdb7a42a529431e682c00c2d77f0b3a
FelixSFD <felixsfd@sobotics.org>
2025-01-26 14:03:07 +0100

    try to add metadata for previews

--- frontend/lp-calendar-web/src/app/concert-details/concert-details.component.ts
+++ frontend/lp-calendar-web/src/app/concert-details/concert-details.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ConcertsService} from '../services/concerts.service';
 import {Concert} from '../data/concert';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import {DateTime} from 'luxon';
 import {NgIf} from '@angular/common';
 import {CountdownComponent} from '../countdown/countdown.component';
+import {Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-concert-details',
@@ -20,7 +21,7 @@ export class ConcertDetailsComponent implements OnInit {
   concert$: Concert | null = null;
   concertId: string | undefined;
 
-  constructor(private route: ActivatedRoute, private concertsService: ConcertsService) {
+  constructor(private route: ActivatedRoute, private concertsService: ConcertsService, private metaService: Meta) {
   }
 
   ngOnInit(): void {
@@ -38,10 +39,49 @@ export class ConcertDetailsComponent implements OnInit {
 
           let titleInfo = result.city + ", " + result.country + concertDateTitleExtension;
           window.document.title = window.document.title.replace("Details", titleInfo);
+
+          this.updateMetaInfo(result);
         }
 
         return this.concert$ = result;
       });
+  }
+
+
+  private updateMetaInfo(concert: Concert) {
+    let pageTitle = "";
+    let description = "";
+
+    let concertDateDescriptionExtension = "";
+    if (concert.postedStartTime != undefined) {
+      let concertDate = new Date(concert.postedStartTime);
+      concertDateDescriptionExtension = concertDate.toLocaleDateString() + ": ";
+    }
+
+    if (concert.tourName != undefined) {
+      pageTitle = concert.tourName + ": " + concert.city;
+      description = concertDateDescriptionExtension + "Linkin Park show of the " + concert.tourName + " in " + concert.city + ", " + concert.country
+    } else {
+      pageTitle = "Linkin Park at " + concert.venue;
+      description = concertDateDescriptionExtension + "Linkin Park show at " + concert.venue + " in " + concert.city + ", " + concert.country
+    }
+
+    this.metaService.updateTag({
+      name: "title",
+      content: pageTitle
+    });
+    this.metaService.updateTag({
+      property: "og:title",
+      content: pageTitle
+    });
+    this.metaService.updateTag({
+      name: "description",
+      content: description
+    });
+    this.metaService.updateTag({
+      property: "og:description",
+      content: description
+    });
   }
 
 
