fca25264d854b392c9974af7c5acb371e5fe629c
FelixSFD <felixsfd@sobotics.org>
2025-01-26 22:08:11 +0100

    try to fix logout

--- frontend/lp-calendar-web/src/environments/environment.github.ts
+++ frontend/lp-calendar-web/src/environments/environment.github.ts
@@ -5,7 +5,6 @@ export const environment = {
   cognitoBaseUrl: "https://COGNITO_URL",
   cognitoClientId: "COGNITO_CLIENT_ID",
   cognitoRedirectUrl: "https://ROOT_DOMAIN",
+  cognitoLogoutUrl: "https://COGNITO_AUTH_SERVER/logout?client_id=COGNITO_CLIENT_ID&logout_uri=https://" + window.document.location.host,
   build: "BUILD_NUMBER"
 };
-
-
