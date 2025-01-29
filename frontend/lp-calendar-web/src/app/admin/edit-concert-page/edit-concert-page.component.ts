6e6dc155c3063d31d94064b474131463c85b587a
FelixSFD <felixsfd@sobotics.org>
2025-01-29 18:09:04 +0100

    fix card layout in concert details and added early entry time

--- frontend/lp-calendar-web/src/app/admin/edit-concert-page/edit-concert-page.component.ts
+++ frontend/lp-calendar-web/src/app/admin/edit-concert-page/edit-concert-page.component.ts
@@ -4,8 +4,6 @@ import {ActivatedRoute, RouterLink} from '@angular/router';
 import {Concert} from '../../data/concert';
 import {ConcertsService} from '../../services/concerts.service';
 import {ToastrService} from 'ngx-toastr';
-import {catchError, throwError} from "rxjs";
-import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-edit-concert-page',
@@ -58,22 +56,5 @@ export class EditConcertPageComponent {
             this.isSaving$ = false;
           }
         });
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-          `Backend returned code ${error.status}, body was: `, error.error);
-    }
-    // Return an observable with a user-facing error message.
-    console.log(this.concertId);
-    this.toastr.error("Invalid fields", "Failed to save concert!");
-
-    return throwError(() => new Error('Something bad happened; please try again later.'));
   }
 }
