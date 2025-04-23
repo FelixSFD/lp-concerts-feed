da54502143a57031e53befabd98c0ec7b47f6219
FelixSFD <felixsfd@sobotics.org>
2025-04-23 20:22:37 +0200

    added missing file

---
+++ frontend/lp-calendar-web/src/app/data/concert-title-generator.spec.ts
@@ -0,0 +1,7 @@
+import { ConcertTitleGenerator } from './concert-title-generator';
+
+describe('ConcertTitleGenerator', () => {
+  it('should create an instance', () => {
+    expect(new ConcertTitleGenerator()).toBeTruthy();
+  });
+});
