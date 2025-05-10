2824d5af309604de7f94e8776dc99e604eeb6cd2
FelixSFD <felixsfd@sobotics.org>
2025-05-10 11:19:56 +0200

    use tooltips in concert-badges.component.html

--- frontend/lp-calendar-web/src/app/concert-badges/concert-badges.component.ts
+++ frontend/lp-calendar-web/src/app/concert-badges/concert-badges.component.ts
@@ -2,12 +2,14 @@ import {Component, Input} from '@angular/core';
 import {NgIf} from "@angular/common";
 import {Concert} from '../data/concert';
 import {defaultShowType} from '../app.config';
+import {NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-concert-badges',
-    imports: [
-        NgIf
-    ],
+  imports: [
+    NgIf,
+    NgbTooltip
+  ],
   templateUrl: './concert-badges.component.html',
   styleUrl: './concert-badges.component.css'
 })
