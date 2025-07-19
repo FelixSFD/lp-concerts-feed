928f075f0c1ae2dbafe7890f59ed1262d86e323c
FelixSFD <felixsfd@sobotics.org>
2025-07-19 23:41:43 +0200

    added methods to check for Update

--- Lambda.Auth/LambdaContextPermissionsExtensions.cs
+++ Lambda.Auth/LambdaContextPermissionsExtensions.cs
@@ -10,10 +10,28 @@ public static class ApiGatewayProxyRequestPermissionsExtensions
     /// <param name="request">Request that contains the claims</param>
     /// <param name="groupName">name of the group</param>
     /// <returns>true, if current user is member of the group</returns>
-    public static bool IsMemberOf(this APIGatewayProxyRequest request, string groupName)
-    {
-        return IsMemberOf(request, [groupName]);
-    }
+    public static bool IsMemberOf(this APIGatewayProxyRequest request, string groupName) 
+        => IsMemberOf(request, [groupName]);
+
+
+    /// <summary>
+    /// Checks in the request if the user is member of a given group or admin
+    /// </summary>
+    /// <param name="request">Request that contains the claims</param>
+    /// <param name="groupName">name of the group</param>
+    /// <returns>true, if current user is member of the group or in the admin group</returns>
+    public static bool IsMemberOfOrAdmin(this APIGatewayProxyRequest request, string groupName) 
+        => IsMemberOf(request, groupName, "Admin");
+    
+    
+    /// <summary>
+    /// Checks in the request if the user is member of a given group or admin
+    /// </summary>
+    /// <param name="request">Request that contains the claims</param>
+    /// <param name="groupNames">name of the groups; Only one of the groups has to match</param>
+    /// <returns>true, if current user is member of the group or in the admin group</returns>
+    public static bool IsMemberOfOrAdmin(this APIGatewayProxyRequest request, params string[] groupNames) 
+        => IsMemberOf(request, groupNames.Append("Admin").ToArray());
     
     
     /// <summary>
@@ -33,15 +51,24 @@ public static class ApiGatewayProxyRequestPermissionsExtensions
             // check if group is included
             .Any(groupNames.Contains);
     }
-    
-    
+
+
     /// <summary>
     /// Checks if the user of the request is allowed to add/publish concerts
     /// </summary>
     /// <param name="request"></param>
     /// <returns></returns>
     public static bool CanAddConcerts(this APIGatewayProxyRequest request)
-        => request.IsMemberOf("AddConcerts", "Admin");
+        => request.IsMemberOfOrAdmin("AddConcerts");
+    
+    
+    /// <summary>
+    /// Checks if the user of the request is allowed to update concerts
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    public static bool CanUpdateConcerts(this APIGatewayProxyRequest request)
+        => request.IsMemberOfOrAdmin("UpdateConcerts");
 
 
     /// <summary>
@@ -50,5 +77,5 @@ public static class ApiGatewayProxyRequestPermissionsExtensions
     /// <param name="request"></param>
     /// <returns></returns>
     public static bool CanManageUsers(this APIGatewayProxyRequest request)
-        => request.IsMemberOf("ManageUsers", "Admin");
+        => request.IsMemberOfOrAdmin("ManageUsers");
 }
