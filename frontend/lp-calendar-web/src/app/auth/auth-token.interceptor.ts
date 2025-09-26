97d89a7b47cab9be2b137def4f05f67fa9e8c183
FelixSFD <felixsfd@icloud.com>
2025-09-26 20:57:51 +0200

    removed debug output

--- frontend/lp-calendar-web/src/app/auth/auth-token.interceptor.ts
+++ frontend/lp-calendar-web/src/app/auth/auth-token.interceptor.ts
@@ -7,21 +7,19 @@ import { switchMap } from 'rxjs/operators';
 import {authRoutePatterns} from './auth.config';
 
 export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log("Intercepting URL", req.url);
   const oidcSecurityService = inject(OidcSecurityService);
 
   let authRequired = false;
   for (let i = 0; i < authRoutePatterns.length; i++) {
     let pattern = authRoutePatterns[i];
     if (pattern.test(req.url)) {
-      console.log(req.url, "requires authentication");
       authRequired = true;
       break;
     }
   }
 
   if (authRequired) {
-    console.log("Authentication will be added...");
+    console.debug("Authentication will be added...");
     return from(oidcSecurityService.getAccessToken()).pipe(
       switchMap((token) => {
         const authReq = req.clone({
@@ -34,6 +32,6 @@ export const authTokenInterceptor: HttpInterceptorFn = (req, next) => {
     );
   }
 
-  // 🚀 If not matched, just pass the request through
+  // If not matched, just pass the request through
   return next(req);
 };
