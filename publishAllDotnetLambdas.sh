3829f8e04f78a0007affde0fe4c5d734ce113f46
FelixSFD <felixsfd@icloud.com>
2025-10-12 20:01:17 +0200

    improved scripts

--- publishAllDotnetLambdas.sh
+++ publishAllDotnetLambdas.sh
@@ -3,14 +3,18 @@
 # Define the pattern for directories to search
 PATTERN="./Lambda.*/src/Lambda.*"
 
-
 # Use `find` to locate directories matching the pattern and loop over them
-find . -type d -path "$PATTERN" -maxdepth 3 | while read -r dir; do
+while read -r dir; do
   echo "Processing directory: $dir"
-  # Navigate to the directory
-  cd "$dir" || { echo "Failed to enter directory: $dir"; continue; }
-  # Run the dotnet lambda package command
-  dotnet lambda package -farch arm64
-  # Return to the previous directory
-  cd - > /dev/null || exit
-done
+  
+  # Extract name of the lambda
+  project_name="${dir##*/}"
+  echo "Project name: $project_name"
+  lambda_name="${project_name##*.}"
+  echo "Short name: $lambda_name"
+  
+  # Call the script that packaged that lambda
+  ./publishDotnetLambda.sh "$lambda_name" || echo "Failed: $dir"
+  
+  echo "Published: $lambda_name"
+done < <(find . -type d -path "$PATTERN" -maxdepth 3)
--- publishDotnetLambda.sh
+++ publishDotnetLambda.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-echo Publishing Lambda.$1 ...
-dotnet lambda package -c Release -farch arm64 --native-aot -ucfb true -pl ./Lambda.$1/src/Lambda.$1/
+project_name="Lambda.$1"
+project_dir="./$project_name/src/$project_name/"
+project_file="$project_dir/$project_name.csproj"
+echo "Project file: $project_file"
+
+echo "Publishing $project_name ..."
+if grep -q LambdaAotProperties "$project_file"; then
+  echo "Project uses AOT"
+  dotnet lambda package -c Release -farch arm64 --native-aot -ucfb true -pl "$project_dir" < /dev/null
+  echo "Exit code: $?"
+else
+  echo "Project uses .NET runtime"
+  dotnet lambda package -farch arm64 -pl "$project_dir" < /dev/null
+  echo "Exit code: $?"
+fi
 
 echo Restore project to fix errors in Rider
-dotnet restore ./Lambda.$1/src/Lambda.$1/Lambda.$1.csproj
+dotnet restore "$project_file"
+echo "Exit code: $?"
 echo Restored project.
