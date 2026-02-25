2ab6f094ee5109d0ce44f493c6f8a57f0e988f51
FelixSFD <felixsfd@icloud.com>
2026-02-25 20:31:09 +0100

    added new sync endpoint

--- Lambda.ListConcerts/src/Lambda.ListConcerts/Function.cs
+++ Lambda.ListConcerts/src/Lambda.ListConcerts/Function.cs
@@ -26,10 +26,59 @@ public class Function(IConcertRepository concertRepository, IConcertBookmarkRepo
         
         if (request.QueryStringParameters != null)
         {
+            if (request is { Path: "/concerts/sync", HttpMethod: "GET" })
+            {
+                context.Logger.LogInformation("Requested sync concerts using new logic.");
+
+                if (request.QueryStringParameters == null 
+                    || !request.QueryStringParameters.TryGetValue("lastSync", out var lastSyncStr))
+                {
+                    context.Logger.LogError("Missing parameter 'lastSync' in query string!");
+                    var error = new ErrorResponse
+                    {
+                        Message = "Missing parameter 'lastSync' in query string!"
+                    };
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = JsonSerializer.Serialize(error, DataStructureJsonContext.Default.ErrorResponse),
+                        Headers = new Dictionary<string, string>
+                        {
+                            { "Content-Type", "application/json" },
+                            { "Access-Control-Allow-Origin", "*" },
+                            { "Access-Control-Allow-Methods", "OPTIONS, POST" }
+                        }
+                    };
+                }
+
+                if (!DateTimeOffset.TryParse(lastSyncStr!, out var lastSync))
+                {
+                    context.Logger.LogError("Failed to parse parameter 'lastSync'!");
+                    var error = new ErrorResponse
+                    {
+                        Message = "Failed to parse parameter 'lastSync'!"
+                    };
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = JsonSerializer.Serialize(error, DataStructureJsonContext.Default.ErrorResponse),
+                        Headers = new Dictionary<string, string>
+                        {
+                            { "Content-Type", "application/json" },
+                            { "Access-Control-Allow-Origin", "*" },
+                            { "Access-Control-Allow-Methods", "OPTIONS, POST" }
+                        }
+                    };
+                }
+
+                return await ReturnSyncResultV2(lastSync, context.Logger);
+            }
+            
             var idParamFound = request.QueryStringParameters.TryGetValue("id", out var searchId);
             if (idParamFound && searchId != null)
             {
                 // Find one concert
+                context.Logger.LogDebug("Requested id: {id}", searchId);
                 return await ReturnSingleConcert(searchId);
             }
             
@@ -60,33 +109,55 @@ public class Function(IConcertRepository concertRepository, IConcertBookmarkRepo
 
         if (request.Path == "/concerts/sync")
         {
-            context.Logger.LogInformation("Requested to sync concerts.");
-            SyncConcertsRequest? syncRequest;
-            try
-            { 
-                syncRequest = JsonSerializer.Deserialize(request.Body, DataStructureJsonContext.Default.SyncConcertsRequest);
-            }
-            catch (Exception exception)
+            if (request.HttpMethod == "POST")
             {
-                context.Logger.LogError(exception, "Failed to parse sync request!");
+                context.Logger.LogInformation("Requested to sync concerts. (OLD logic!)");
+                SyncConcertsRequest? syncRequest;
+                try
+                { 
+                    syncRequest = JsonSerializer.Deserialize(request.Body, DataStructureJsonContext.Default.SyncConcertsRequest);
+                }
+                catch (Exception exception)
+                {
+                    context.Logger.LogError(exception, "Failed to parse sync request!");
+                    var error = new ErrorResponse
+                    {
+                        Message = exception.Message
+                    };
+                    return new APIGatewayProxyResponse
+                    {
+                        StatusCode = 400,
+                        Body = JsonSerializer.Serialize(error, DataStructureJsonContext.Default.ErrorResponse),
+                        Headers = new Dictionary<string, string>
+                        {
+                            { "Content-Type", "application/json" },
+                            { "Access-Control-Allow-Origin", "*" },
+                            { "Access-Control-Allow-Methods", "OPTIONS, POST" }
+                        }
+                    };
+                }
+
+                return await ReturnSyncResult(syncRequest!, context.Logger);
+            }
+            else
+            {
+                context.Logger.LogError("Method '{method}' not supported for /sync endpoint!");
                 var error = new ErrorResponse
                 {
-                    Message = exception.Message
+                    Message = "Method not supported for sync."
                 };
                 return new APIGatewayProxyResponse
                 {
-                    StatusCode = 404,
+                    StatusCode = 405,
                     Body = JsonSerializer.Serialize(error, DataStructureJsonContext.Default.ErrorResponse),
                     Headers = new Dictionary<string, string>
                     {
                         { "Content-Type", "application/json" },
                         { "Access-Control-Allow-Origin", "*" },
-                        { "Access-Control-Allow-Methods", "OPTIONS, GET" }
+                        { "Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE" }
                     }
                 };
             }
-
-            return await ReturnSyncResult(syncRequest!, context.Logger);
         }
         
         if (request.Path is "/concerts/attending" or "/concerts/bookmarked")
@@ -283,6 +354,37 @@ public class Function(IConcertRepository concertRepository, IConcertBookmarkRepo
         };
         
         logger.LogDebug("Finished sync. Added: {addCount}; Changed: {changeCount}; Deleted: {deleteCount}; SyncTime: {syncTime}", responseObj.Added.Length, responseObj.Updated.Length, responseObj.DeletedConcertIds.Length, syncTime);
+        
+        var json = JsonSerializer.Serialize(responseObj, DataStructureJsonContext.Default.SyncConcertsResponse);
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = 200,
+            Body = json,
+            Headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" },
+                { "Access-Control-Allow-Origin", "*" },
+                { "Access-Control-Allow-Methods", "OPTIONS, GET, POST" }
+            }
+        };
+    }
+    
+    
+    private async Task<APIGatewayProxyResponse> ReturnSyncResultV2(DateTimeOffset lastSync, ILambdaLogger logger)
+    {
+        var syncTime = DateTimeOffset.UtcNow;
+        logger.LogDebug("Sync Concerts at: {syncTime}", syncTime);
+        var syncEngine = new ConcertSyncEngine(concertRepository);
+        var syncResult = await syncEngine.ChangesSince(lastSync);
+        var responseObj = new SyncConcertsResponse
+        {
+            Added = [], // unused will be removed at some point
+            Updated = syncResult.ChangedObjects.ToArray(),
+            DeletedConcertIds = syncResult.DeletedIds.ToArray(),
+            SyncTime = syncTime
+        };
+        
+        logger.LogDebug("Finished sync. Changed: {changeCount}; Deleted: {deleteCount}; SyncTime: {syncTime}", responseObj.Added.Length, responseObj.Updated.Length, responseObj.DeletedConcertIds.Length, syncTime);
         
         var json = JsonSerializer.Serialize(responseObj, DataStructureJsonContext.Default.SyncConcertsResponse);
         return new APIGatewayProxyResponse
