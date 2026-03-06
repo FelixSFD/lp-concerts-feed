0425ffa8751ce3e7f1e788b17c4b2e53df2e9694
FelixSFD <felixsfd@icloud.com>
2026-03-06 19:47:21 +0100

    added remaining tables

--- Database.Setlists/DbScripts/0001_InitDb.sql
+++ Database.Setlists/DbScripts/0001_InitDb.sql
@@ -21,6 +21,20 @@ create table SongVariant (
     FOREIGN KEY (SongId) REFERENCES Song(Id)
 );
 
+create table SongMashup (
+     Id int primary key AUTO_INCREMENT,
+     Title varchar(31),
+     LinkinpediaUrl varchar(63)
+);
+
+create table SongInMashup (
+    SongId int,
+    MashupId int,
+    CONSTRAINT PK_SetlistAct PRIMARY KEY (SongId,MashupId),
+    FOREIGN KEY (SongId) REFERENCES Song(Id),
+    FOREIGN KEY (MashupId) REFERENCES SongMashup(Id)
+);
+
 
 create table Setlist (
     Id int primary key AUTO_INCREMENT,
@@ -51,4 +65,14 @@ create table SetlistEntry (
     FOREIGN KEY (SetlistId, ActNumber) REFERENCES SetlistAct(SetlistId, ActNumber),
     FOREIGN KEY (PlayedSongId) REFERENCES Song(Id),
     FOREIGN KEY (PlayedVariantId) REFERENCES SongVariant(Id)
+);
+
+create table SetlistEntrySongExtra (
+      Id varchar(63) primary key not null,
+      SetlistEntryId varchar(63) not null,
+      SongId int,
+      Type varchar(31),
+      Description varchar(127),
+      FOREIGN KEY (SetlistEntryId) REFERENCES SetlistEntry(Id),
+      FOREIGN KEY (SongId) REFERENCES Song(Id)
 );
