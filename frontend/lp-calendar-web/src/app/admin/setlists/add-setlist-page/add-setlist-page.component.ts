a401101d01c220f1ca982c2d15870ad26e393094
FelixSFD <felixsfd@icloud.com>
2026-03-13 17:15:28 +0100

    improve form

--- frontend/lp-calendar-web/src/app/admin/setlists/add-setlist-page/add-setlist-page.component.ts
+++ frontend/lp-calendar-web/src/app/admin/setlists/add-setlist-page/add-setlist-page.component.ts
@@ -1,7 +1,10 @@
 import {Component, inject, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import {NgClass} from '@angular/common';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {CreateSetlistRequestDto, ErrorResponseDto} from '../../../modules/lpshows-api';
+import {SetlistsService} from '../../../services/setlists.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-setlist-page',
@@ -16,6 +19,7 @@ import {ActivatedRoute} from '@angular/router';
 export class AddSetlistPageComponent implements OnInit {
   private formBuilder = inject(FormBuilder);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   setlistForm = this.formBuilder.group({
     concertId: new FormControl('', [Validators.required]),
@@ -23,6 +27,9 @@ export class AddSetlistPageComponent implements OnInit {
   });
 
   isSaving$: boolean = false;
+
+  constructor(private setlistService: SetlistsService, private toastr: ToastrService) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
@@ -34,8 +41,67 @@ export class AddSetlistPageComponent implements OnInit {
     })
   }
 
+  private makeRequestDtoFromFormData(): CreateSetlistRequestDto {
+    let concertId = this.setlistForm.getRawValue().concertId?.valueOf();
+    let linkinpediaUrl = this.setlistForm.value.linkinpediaUrl?.valueOf();
+
+    console.log('concertId', concertId);
+    console.log('linkinpediaUrl', linkinpediaUrl);
+
+    let request: CreateSetlistRequestDto = {
+      concertId: concertId,
+      linkinpediaUrl: linkinpediaUrl,
+    };
+
+    console.debug("Request: ", request);
+
+    return request;
+  }
+
   onSaveClicked() {
     this.isSaving$ = true;
-    this.isSaving$ = false;
+    let request = this.makeRequestDtoFromFormData();
+    this.setlistService.createSetlist(request)
+      .subscribe({
+        next: (response) => {
+          let toast = this.toastr.success("Setlist was created successfully");
+          toast.onTap.subscribe(toast => {
+            this.navigateToSetlist(response.id);
+          });
+
+          this.navigateToSetlist(response.id);
+
+          this.isSaving$ = false;
+          this.setlistForm.reset();
+        },
+        error: err => {
+          let errorResponse: ErrorResponseDto = err.error;
+          this.toastr.error(errorResponse.message, "Could not create setlist");
+          this.isSaving$ = false;
+        }
+      });
+  }
+
+  private navigateToSetlist(id: string | undefined) {
+    this.router.navigate(['/admin/setlists/' + id]).catch(err => this.toastr.error(err));
+  }
+
+  openConcertDetailsClicked() {
+    let concertId = this.setlistForm.getRawValue().concertId?.valueOf();
+    if (concertId?.length == 0) {
+      return;
+    }
+
+    window.open("/concerts/" + concertId, "_blank");
+  }
+
+
+  openLinkinpediaUrlClicked() {
+    let url = this.setlistForm.value.linkinpediaUrl?.valueOf();
+    if (url?.length == 0) {
+      return;
+    }
+
+    window.open(url, "_blank");
   }
 }
