8a589546f14aeb7d0ea282fb792c967378f8e468
FelixSFD <felixsfd@icloud.com>
2026-04-19 11:47:26 +0200

    fix unit test

--- Lambda.Push.Sender/test/Lambda.Push.Sender/NotificationJsonSerializerTest.cs
+++ Lambda.Push.Sender/test/Lambda.Push.Sender/NotificationJsonSerializerTest.cs
@@ -25,8 +25,8 @@ public class NotificationJsonSerializerTest
     
     
     [Theory]
-    [InlineData("Stage Time Confirmed", "Some Body", "1234567", true, "confirmedStageTime", "1234")]
-    public void StageTimeConfirmed(string title, string body, string threadId, bool isMutable, string category, string concertId)
+    [InlineData("Stage Time Confirmed", "Some subtitle", "Some Body", "1234567", true, "confirmedStageTime", "1234")]
+    public void StageTimeConfirmed(string title, string subtitle, string body, string threadId, bool isMutable, string category, string concertId)
     {
         var payload = new NotificationWrapper<AppleNotificationAlert>
         {
@@ -35,6 +35,7 @@ public class NotificationJsonSerializerTest
                 Alert = new AppleNotificationPayload
                 {
                     Title = title,
+                    Subtitle = subtitle,
                     Body = body
                 },
                 ThreadId = threadId,
@@ -45,7 +46,7 @@ public class NotificationJsonSerializerTest
         };
 
         var json = JsonSerializer.Serialize(payload, NotificationJsonSerializer.Default.NotificationWrapperAppleNotificationAlert);
-        var expectedJson = "{\"aps\":{\"alert\":{\"title\":\"" + title + "\",\"body\":\"" + body + "\"},\"thread-id\":\"" + threadId + "\",\"category\":\"" + category + "\",\"mutable-content\":" + (isMutable ? 1 : 0) + "},\"concertId\":\"" + concertId + "\"}";
+        var expectedJson = "{\"aps\":{\"alert\":{\"title\":\"" + title + "\",\"subtitle\":\"" + subtitle + "\",\"body\":\"" + body + "\"},\"thread-id\":\"" + threadId + "\",\"category\":\"" + category + "\",\"mutable-content\":" + (isMutable ? 1 : 0) + "},\"concertId\":\"" + concertId + "\"}";
         Assert.Equal(expectedJson, json);
     }
 }
