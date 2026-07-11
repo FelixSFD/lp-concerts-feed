ec59b915097adf02693a893f16d793aeca781443
FelixSFD <felixsfd@icloud.com>
2026-07-11 12:19:36 +0200

    added test for errors

--- Service.Tours.Tests/LocationServiceTest.cs
+++ Service.Tours.Tests/LocationServiceTest.cs
@@ -2,21 +2,22 @@
 using Database.Tours.Repositories;
 using Microsoft.Extensions.Logging;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
 using NSubstitute.Extensions;
+using Service.Tours.Exceptions;
 
 namespace Service.Tours.Tests;
 
 public class LocationServiceTest
 {
-    private ILogger<LocationService> _logger;
-    private ICountryRepository _countryRepository;
-    private LocationService _service;
+    private readonly ICountryRepository _countryRepository;
+    private readonly LocationService _service;
 
     public LocationServiceTest()
     {
-        _logger = Substitute.For<ILogger<LocationService>>();
+        var logger = Substitute.For<ILogger<LocationService>>();
         _countryRepository = Substitute.For<ICountryRepository>();
-        _service = new LocationService(_countryRepository, _logger);
+        _service = new LocationService(_countryRepository, logger);
     }
     
     [Theory]
@@ -50,5 +51,25 @@ public class LocationServiceTest
         await _countryRepository
             .Received(1)
             .GetByPrimaryKeyAsync(Arg.Is(isoCode));
+    }
+    
+    [Fact]
+    public async Task GetCountry_ByIsoCode_NotFound()
+    {
+        // setup mocks
+        _countryRepository
+            .Configure()
+            .GetByPrimaryKeyAsync("AAA")
+            .ThrowsAsync(new CountryNotFoundException("AAA"));
+
+        // Call the service
+        var exception = await Assert.ThrowsAsync<CountryNotFoundException>(async () => await _service.GetCountry("AAA"));
+        Assert.NotNull(exception);
+        Assert.Equal("AAA", exception.CountryCode);
+        
+        // verify mock calls
+        await _countryRepository
+            .Received(1)
+            .GetByPrimaryKeyAsync(Arg.Is("AAA"));
     }
 }
--- Service.Tours/Exceptions/CountryNotFoundException.cs
+++ Service.Tours/Exceptions/CountryNotFoundException.cs
@@ -2,4 +2,8 @@ namespace Service.Tours.Exceptions;
 
 public class CountryNotFoundException(string countryCode) : Exception($"Country '{countryCode}' not found!")
 {
+    /// <summary>
+    /// ISO Code of the country that could not be found
+    /// </summary>
+    public string CountryCode { get; } = countryCode;
 }
