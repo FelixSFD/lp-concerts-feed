c15fc940605ceb43fc7b2147e84e454d8dbae52e
FelixSFD <felixsfd@icloud.com>
2026-08-02 21:01:10 +0200

    test if compile runs before the script

---
+++ Common.Contracts/TestGenerated.cs
@@ -0,0 +1,11 @@
+using Common.Contracts.Generated.Models;
+
+namespace Common.Contracts;
+
+/// <summary>
+/// only to check if compiler runs before the script
+/// </summary>
+public class TestGenerated
+{
+    public CountryDto CountryDto { get; set; }
+}
