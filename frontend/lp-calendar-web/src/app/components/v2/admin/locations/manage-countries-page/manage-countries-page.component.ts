977109db46713676772ca8c70cff6e25553fbf0a
FelixSFD <felixsfd@icloud.com>
2026-08-02 18:28:03 +0200

    added list of countries

--- frontend/lp-calendar-web/src/app/components/v2/admin/locations/manage-countries-page/manage-countries-page.component.ts
+++ frontend/lp-calendar-web/src/app/components/v2/admin/locations/manage-countries-page/manage-countries-page.component.ts
@@ -4,10 +4,32 @@ import { SongsService } from '../../../../../services/songs.service';
 import { ErrorResponseDto, SongDto } from '../../../../../modules/lpshows-api';
 import { CountryDto } from '../../../../../modules/lpshows-api/v3';
 import { LocationsService } from '../../../../../services/locations.service';
+import { Button } from 'primeng/button';
+import { ButtonGroup } from 'primeng/buttongroup';
+import { Card } from 'primeng/card';
+import { ConfirmDialog } from 'primeng/confirmdialog';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { InputText } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TableModule } from 'primeng/table';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-manage-countries-page',
-  imports: [],
+  imports: [
+    Button,
+    ButtonGroup,
+    Card,
+    ConfirmDialog,
+    IconField,
+    InputIcon,
+    InputText,
+    ReactiveFormsModule,
+    TableModule,
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './manage-countries-page.component.html',
   styleUrl: './manage-countries-page.component.css',
 })
