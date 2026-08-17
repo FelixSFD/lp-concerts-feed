cd259d373fb6d5e8d52c216204d8347fd8688437
FelixSFD <felixsfd@icloud.com>
2026-08-17 15:03:51 +0200

    fixed bug with tour filter

--- frontend/lp-calendar-web/src/app/components/v2/tour-map-page/tour-map-page.component.ts
+++ frontend/lp-calendar-web/src/app/components/v2/tour-map-page/tour-map-page.component.ts
@@ -35,7 +35,7 @@ export class TourMapPageComponent {
   // default filter that is used when loading the list
   defaultFilter: ConcertFilter = {
     onlyFuture: false,
-    tour: "FROM ZERO WORLD TOUR 2026",
+    tour: tourConfigs.find(t => t.value == "FROM ZERO WORLD TOUR 2026"),
     dateFrom: DateTime.fromISO("0000-01-01T00:00:00.000Z"),
     dateTo: DateTime.fromISO("3000-12-31T23:59:59.999Z"),
   };
