5a726b949103944f6cba19ee2395d80e533af321
FelixSFD <felixsfd@icloud.com>
2026-09-12 20:55:06 +0200

    fixed logger for lambda

--- Common.WikiMedia/Repositories/WikiMediaRepository.cs
+++ Common.WikiMedia/Repositories/WikiMediaRepository.cs
@@ -11,7 +11,7 @@ namespace Common.WikiMedia.Repositories;
 /// <param name="httpClient"></param>
 /// <param name="restApiBaseUrl">Base URL for the REST-API</param>
 /// <param name="restApiBaseUrl">Base URL for the Action-API</param>
-public class WikiMediaRepository(HttpClient httpClient, string restApiBaseUrl, string actionApiBaseUrl, ILogger<WikiMediaRepository> logger) : IWikiMediaRepository
+public class WikiMediaRepository(HttpClient httpClient, string restApiBaseUrl, string actionApiBaseUrl, ILogger<WikiMediaRepository>? logger) : IWikiMediaRepository
 {
     /// <summary>
     /// Helper to generate the API URLs for the REST API
@@ -40,23 +40,23 @@ public class WikiMediaRepository(HttpClient httpClient, string restApiBaseUrl, s
     public async IAsyncEnumerable<CargoQueryResponseItemDto<T>> RunCargoQueryAsync<T>(string[] tables, string[] fields, CargoQueryWhereClause[] where,
         string[] orderBy, int pageSize = 200, [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        logger.LogDebug("Starting cargo query: Tables: {tables}; Fields: {fields}; Where: {where}; Order by: {orderBy}; Page size: {pageSize}", tables, fields, where, orderBy, pageSize);
+        logger?.LogDebug("Starting cargo query: Tables: {tables}; Fields: {fields}; Where: {where}; Order by: {orderBy}; Page size: {pageSize}", tables, fields, where, orderBy, pageSize);
         
         var offset = 0;
         while (true)
         {
             if (cancellationToken.IsCancellationRequested)
             {
-                logger.LogDebug("Cargo query was cancelled.");
+                logger?.LogDebug("Cargo query was cancelled.");
                 break;
             }
             
             var url = _actionApiUrlBuilder.GetCargoQueryUrl(tables, fields, where, orderBy, pageSize, offset);
-            logger.LogDebug("Get next page from URL: {url}", url);
+            logger?.LogDebug("Get next page from URL: {url}", url);
             var queryResponse = await httpClient.GetFromJsonAsync<CargoQueryResponseDto<T>>(url, cancellationToken);
             if (queryResponse == null)
             {
-                logger.LogWarning("Cargo query returned null. Maybe JSON parsing failed?");
+                logger?.LogWarning("Cargo query returned null. Maybe JSON parsing failed?");
                 break;
             }
 
@@ -64,7 +64,7 @@ public class WikiMediaRepository(HttpClient httpClient, string restApiBaseUrl, s
             {
                 if (cancellationToken.IsCancellationRequested)
                 {
-                    logger.LogDebug("Cargo query was cancelled.");
+                    logger?.LogDebug("Cargo query was cancelled.");
                     break;
                 }
                 
@@ -73,14 +73,14 @@ public class WikiMediaRepository(HttpClient httpClient, string restApiBaseUrl, s
             
             if (queryResponse.Results.Length != pageSize)
             {
-                logger.LogDebug("Cargo query returned less results than requested. This is the last page.");
+                logger?.LogDebug("Cargo query returned less results than requested. This is the last page.");
                 break;
             }
             
             offset += pageSize;
-            logger.LogDebug("Next page offset: {offset}", offset);
+            logger?.LogDebug("Next page offset: {offset}", offset);
         }
         
-        logger.LogDebug("Finished cargo query and returned all results from all pages.");
+        logger?.LogDebug("Finished cargo query and returned all results from all pages.");
     }
 }
